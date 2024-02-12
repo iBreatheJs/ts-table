9290e6e1beebcfda78c59d401bb05698497c06af
hsz <sdfghjk>
2024-02-12 01:44:26 +0100

    add css and update widget to work with cdb

--- src/widget.ts
+++ src/widget.ts
@@ -1,11 +1,15 @@
-import { Table, TableConstructor } from "./table";
-import { TableContainer, TableData, TableParams, TableParamsTuple } from "./types";
-import { IWidgetRequirements, IWidgetRequirementsNew, SimpleWidget, Widget, WidgetConfig, WidgetInfo, WidgetInfoFrom, WidgetRequirements } from "../../../widget";
+import { Table } from "./table";
+import { TableContainer, TableData, TableOptions, TableParams, TableParamsTuple } from "./types";
+// import { SimpleWidget, Widget, WidgetConfig, WidgetInfo, WidgetParams } from "@lib/../../code/temp/cdbfix/cdb/src/ts/widget";
+import { SimpleWidget, Widget, WidgetConfig, WidgetInfo, WidgetParams } from "../../../widget";
+// import { WidgetParams } from "../../../widget";
+// import { IWidgetRequirements, IWidgetRequirementsNew, S0impleWidget, Widget, WidgetConfig, WidgetInfo, WidgetInfoFrom, WidgetRequirements } from "../../../widget";
 import { Wrapper } from "../../../wrapper";
 
 import * as info from "./info.json";
 import { testData } from "../data/test-data";
-type Info = WidgetInfoFrom<typeof info>
+import { Cdb } from "../../../cdb/cdb";
+// type Info = WidgetInfoFrom<typeof info>
 
 // use mixins or some type of factory... 
 // goal is to have a table cls that can be used wherever (independent of cdb)
@@ -14,16 +18,72 @@ type Info = WidgetInfoFrom<typeof info>
 
 
 
-export class TableWidget<T extends TableData> extends Table<T> implements IWidgetRequirements {
+// export class TableWidget<T extends TableData> extends Table<T> implements IWidgetRequirements {
+//     static info: Info = info // todo; its not required here
+//     info: WidgetInfo = info
+
+//     constructor(args: Omit<TableParams<T>, 'container'>) {
+//         // constructor(specificTableShit: string, ...args: ConstructorParameters<typeof Table<T>>) {
+//         let container = widget.wrapper.div // todo: double, already assign it as arg ? matter of syntax: override when creating a widget or define fixed order arg as container?? do widgets need contaiers always?? todotodo
+//         let defaultArgs: TableParams<typeof testData.simple.data> = { data: testData.simple.data, header: testData.simple.header, container: testData.simple.container }
+//         // super(...args)
+//         // super(testData.simple.container, testData.simple.data)
+//         console.log("widget");
+//         if (!args) {
+//             throw new Error("no args");
+
+//         }
+//         // args[0] = container
+//         console.log(args);
+//         console.log("widgetttt");
+//         // let argsTest = { container: container, data: testData.simple.data as unknown as T }
+//         // super(...args)
+//         super({ ...args, container: container })
+//         this.actions
+//         this.draw()
+//     }
+//     act = {
+//         "add-row": this.actions["add-row"],
+//         sort: this.actions.sort
+//     }
+// }
+export class TableWidget<T extends TableData> extends Table<T>{
+
+    // static info: Info = info // todo; its not required here
+    // info: WidgetInfo = info
+    static info = {
+        id: "tableWidget",
+        name: "tableWidget",
+        version: 2
+    }
+    constructor(params: WidgetParams['inst'], parent?: Cdb | undefined) {
+        let tableParams: TableParams<T> = {
+            data: params?.options.data,
+            container: params?.container,
+            options: params?.options as TableOptions<T>
+        }
+        super(tableParams)
+        let container = params?.container
+        let defaultArgs: TableParams<typeof testData.simple.data> = { data: testData.simple.data, header: testData.simple.header, container: testData.simple.container }
+        this.actions
+        this.draw()
+    }
+    act = {
+        "add-row": this.actions["add-row"],
+        sort: this.actions.sort
+    }
+}
+
+Widget.list.push(TableWidget) // todo: figure out how to add widgets to the list
+
+/**
+ * idk what i fugured out there exactly a mil y ago.
+ * but ill try to just inherit from widget, instantiate table and pass things thru.
+ */
+export class TableWidgetOld<T extends TableData> extends Table<T> implements IWidgetRequirements {
     static info: Info = info // todo; its not required here
     info: WidgetInfo = info
 
-    // with custom constructor the constructor overloads are lost but can have additional params
-    // constructor(...args: ConstructorParameters<typeof Table<T>>) {
-    // constructor(...args: ConstructorParameters<typeof Table<T>>) {
-    // constructor(widget: Widget<IWidgetRequirementsNew>, ...args: ConstructorParameters<typeof Table>) {
-    //that shit works but dont need container here
-    // constructor(widget: Widget<IWidgetRequirementsNew>, ...args: TableParamsTuple<T>) {
     constructor(widget: Widget<IWidgetRequirementsNew>, args: Omit<TableParams<T>, 'container'>) {
         // constructor(specificTableShit: string, ...args: ConstructorParameters<typeof Table<T>>) {
         let container = widget.wrapper.div // todo: double, already assign it as arg ? matter of syntax: override when creating a widget or define fixed order arg as container?? do widgets need contaiers always?? todotodo
@@ -43,14 +103,4 @@ export class TableWidget<T extends TableData> extends Table<T> implements IWidge
         super({ ...args, container: container })
         this.draw()
     }
-
-
 }
-
-// testData
-// let asdf = new TableWidget(testData.simple.container, testData.simple.data)
-// // the object constructor option is lost but thats as much as imma fk around w this ts rn
-// let asdf2 = new TableWidget({ container: testData.simple.container, data: testData.simple.data })
-// asdf2.info
-// let asdftable = new Table(testData.simple.container, testData.simple.data)
-// let asdftable2 = new Table({ container: testData.simple.container, data: testData.simple.data })
